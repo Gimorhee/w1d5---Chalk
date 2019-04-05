61007e96139378cea4b7afd32586aab6fb3a0dd7
Danny Rhee <dongyunrhee@gmail.com>
2019-04-05 16:03:15 +0000

    npm and package.json - installing Chalk assignment.

---
+++ index.js
@@ -0,0 +1,11 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+console.log(message);
+
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
